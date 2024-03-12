99055d0bd24e3c268b8891f0ac310e9b6a59a889
PakinWasu <pakin.wasu2002@gmail.com>
2024-03-12 08:26:16 +0700

    Fainal

--- commander.py
+++ commander.py
@@ -10,13 +10,12 @@ sqlfile = 'DBlite.db'
 
 # insertdb.insert_order('C00001')
 
-# insertdb.insert_orderdetail('S00008','P00001',2)
+# insertdb.insert_orderdetail('S00009','P00001',2)
  
-#updatedb.update_slip_payment('S00008','MYMO','D:\DBProject_Frist_Time\picpro\slip3.jpg')
-#updatedb.update_vertify_payment('S00008')
+# updatedb.update_slip_payment('S00009','MYMO','D:\DBProject_Frist_Time\picpro\slip3.jpg')
+# updatedb.update_vertify_payment('S00009')
 
 
-# updatedb.update_price_of_product('P00001','900')
+# updatedb.update_price_of_product('P00001','958')
 #updatedb.update_plus_value_product('P00001',5)
-
-# deldb.del_order('S00008')
+deldb.del_order('S00010')
